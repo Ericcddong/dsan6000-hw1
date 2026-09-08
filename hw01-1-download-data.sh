dc802e7e7c5fb6721e46cfd6248336a72f3b6060
Ubuntu <ubuntu@ip-172-31-39-114.ec2.internal>
2026-09-08 17:10:33 +0000

    copy csv files

--- hw01-1-download-data.sh
+++ hw01-1-download-data.sh
@@ -1,1 +1,8 @@
-ls
+# list remote data files
+aws s3 ls s3://dsan6000-wikipedia/hourly/
+
+# copy remote data into local data folder
+aws s3 cp s3://dsan6000-wikipedia/hourly/ data/ --recursive --exclude "*" --include "*.csv"
+
+# list local data files
+ls data/*.csv
